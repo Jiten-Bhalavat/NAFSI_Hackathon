4528ae41571ccc96d8167e4e7f62c2d08acf9708
protocorn <chordiasahil24@gmail.com>
2026-04-13 15:22:38 -0400

    deploy

--- nourishnet/vite.config.ts
+++ nourishnet/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/nourishnet/" : "/",
+  base: process.env.GITHUB_ACTIONS ? "/NAFSI_Track2/" : "/",
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ["supercluster", "maplibre-gl"],
